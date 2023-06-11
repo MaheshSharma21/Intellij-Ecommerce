e0e78cda52d1dbd0f615ff4dba9d68eeb96e1336
MaheshSharma21 <maheshsharma127201@gmail.com>
2023-06-11 22:08:13 +0530

    Category module entity class updated

---
+++ src/main/java/com/bikkadit/electronic/store/entities/Category.java
@@ -0,0 +1,27 @@
+package com.bikkadit.electronic.store.entities;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "categories")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String categoryId;
+
+    @Column(name = "category_title")
+    private String title;
+
+    @Column(name = "category_description")
+    private String description;
+
+    private String coverImage;
+}
