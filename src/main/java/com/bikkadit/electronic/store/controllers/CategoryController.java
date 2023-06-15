768746ef3cf17ea9445c84d8555fec6493513d99
MaheshSharma21 <maheshsharma127201@gmail.com>
2023-06-15 08:24:23 +0530

    controller method calling for coverImage

--- src/main/java/com/bikkadit/electronic/store/controllers/CategoryController.java
+++ src/main/java/com/bikkadit/electronic/store/controllers/CategoryController.java
@@ -2,17 +2,27 @@ package com.bikkadit.electronic.store.controllers;
 
 import com.bikkadit.electronic.store.helper.ApiResponse;
 import com.bikkadit.electronic.store.helper.AppConstant;
+import com.bikkadit.electronic.store.helper.ImageResponse;
 import com.bikkadit.electronic.store.helper.PageableResponse;
 import com.bikkadit.electronic.store.payloads.CategoryDto;
+import com.bikkadit.electronic.store.payloads.UserDto;
 import com.bikkadit.electronic.store.service.CategoryServiceI;
 
+import com.bikkadit.electronic.store.service.FileServiceI;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @Slf4j
@@ -22,6 +32,12 @@ public class CategoryController {
 
     @Autowired
     private CategoryServiceI categoryServiceI;
+
+    @Autowired
+    private FileServiceI fileServiceI;
+
+    @Value("${category.profile.image.path}")
+    private String uploadCoverImagePath;
 
     /**
      * @param categoryDto
@@ -114,5 +130,49 @@ public class CategoryController {
         List<CategoryDto> categoryDto= this.categoryServiceI.searchCategoryByTitle(keyword);
         log.info("Request completed for service layer to search category with keyword :{}", keyword);
         return new ResponseEntity<>(categoryDto, HttpStatus.FOUND);
+    }
+
+
+
+
+    /**
+     * @param file
+     * @param categoryId
+     * @return
+     * @throws IOException
+     * @author Mahesh Sharma
+     * @apiNote This api is used to upload coverImage for category
+     */
+    @PostMapping("/category/image/{categoryId}")
+    public ResponseEntity<ImageResponse> uploadCoverImage(@RequestParam("categoryImage") MultipartFile file, @PathVariable String categoryId) throws IOException {
+        log.info(" Request Starting for fileService layer to upload coverImage with categoryId :{}", categoryId);
+        String ImageName = fileServiceI.uploadcoverImage(file, uploadCoverImagePath);
+
+        CategoryDto category = categoryServiceI.getCategoryById(categoryId);
+
+        category.setCoverImage(ImageName);
+        CategoryDto userDto = categoryServiceI.updateCategory(category, categoryId);
+        ImageResponse fileUploaded = ImageResponse.builder().imageName(ImageName).message("File uploaded ").success(true).Status(HttpStatus.CREATED).build();
+        log.info(" Request completed for fileService layer to upload coverImage with categoryId :{}", categoryId);
+        return new ResponseEntity<>(fileUploaded, HttpStatus.CREATED);
+    }
+
+    /**
+     * @param categoryId
+     * @param response
+     * @throws IOException
+     * @author Mahesh Sharma
+     * @apiNote This api is used to server Image
+     */
+    @GetMapping("category/image/{categoryId}")
+    public void serverImage(@PathVariable String categoryId, HttpServletResponse response) throws IOException {
+        log.info(" Request Starting for fileService layer to serve coverImage with categoryId :{}", categoryId);
+        CategoryDto user = categoryServiceI.getCategoryById(categoryId);
+        log.info(" user Image Name :{}", user.getCoverImage());
+        InputStream resource = fileServiceI.getcoverImage(uploadCoverImagePath, user.getCoverImage());
+
+        response.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource, response.getOutputStream());
+        log.info(" Request completed for fileService layer to serve coverImage with categoryId :{}", categoryId);
     }
 }
