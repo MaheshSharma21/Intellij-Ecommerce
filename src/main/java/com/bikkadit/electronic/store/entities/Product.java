f69e48217e7e3926622e4d84f8d4a2f72e4716ea
MaheshSharma21 <maheshsharma127201@gmail.com>
2023-06-16 12:30:23 +0530

    Product entity added

---
+++ src/main/java/com/bikkadit/electronic/store/entities/Product.java
@@ -0,0 +1,44 @@
+package com.bikkadit.electronic.store.entities;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.util.Date;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name="products")
+public class Product {
+
+    @Id
+    private String productId;
+    @Column(name="product_title")
+    private String title;
+    @Column(name="product_description")
+    private String description;
+    @Column(name="product_price")
+    private Integer price;
+    @Column(name="product_quantity")
+    private Integer quantity;
+    @Column(name="product_weight")
+    private Integer weight;
+    @Column(name="product_colour")
+    private String colour;
+    @Column(name="product_materialUsed")
+    private String materialUsed;
+    @Column(name="product_added_date")
+    private Date addedDate;
+    @Column(name="product_isLive")
+    private boolean live;
+    @Column(name="product_isAvailable")
+    private boolean stock;
+
+
+}
