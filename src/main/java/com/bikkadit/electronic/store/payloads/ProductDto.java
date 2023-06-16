2bb9766a4d4d467a46cbd9fd405dbc93e66dd485
MaheshSharma21 <maheshsharma127201@gmail.com>
2023-06-16 12:56:48 +0530

    Product dto related changes

---
+++ src/main/java/com/bikkadit/electronic/store/payloads/ProductDto.java
@@ -0,0 +1,45 @@
+package com.bikkadit.electronic.store.payloads;
+
+import lombok.*;
+
+
+import javax.validation.constraints.*;
+import java.util.Date;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class ProductDto {
+
+    private String productId;
+
+    @Size(min=5 ,max=20,message = "Product title must contains at least 5 characters and max to max 20 characters")
+    private String title;
+
+    @Max(value = 50,message = " You can add description up to 50 length ")
+    private String description;
+
+    @NotNull(message = "product price is required ....")
+    private Integer price;
+
+    @NotNull(message = " please enter how much product quantity you want ")
+    private Integer quantity;
+
+    @NotNull(message = " you can buy product up to  50kg weight")
+    private Integer weight;
+
+    @NotBlank(message = " colours available for product are Red , skyBlue , pink , Blue ")
+    @Size(max = 10 ,message = " colours name above 10 characters  are not acceptable ")
+    private String colour;
+
+    private Date addedDate;
+
+    @NotBlank(message = " product live field is mandatory to fill ....")
+    @Size(min =4,max =5 ,message = " product live size  exists  within 4-5 range ")
+    private boolean live;
+
+    private boolean stock = true;
+
+}
