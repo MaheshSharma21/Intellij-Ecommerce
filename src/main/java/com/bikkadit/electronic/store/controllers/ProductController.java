7087e0a2722d76c472b36a5451b987cffbfb7360
MaheshSharma21 <maheshsharma127201@gmail.com>
2023-06-16 18:51:53 +0530

    All required changes related to implementation

---
+++ src/main/java/com/bikkadit/electronic/store/controllers/ProductController.java
@@ -0,0 +1,13 @@
+package com.bikkadit.electronic.store.controllers;
+
+import com.bikkadit.electronic.store.payloads.ProductDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ProductController {
+
+
+}
---
+++ src/main/java/com/bikkadit/electronic/store/service/ProductServiceI.java
@@ -0,0 +1,21 @@
+package com.bikkadit.electronic.store.service;
+
+import com.bikkadit.electronic.store.helper.PageableResponse;
+import com.bikkadit.electronic.store.payloads.ProductDto;
+
+public interface ProductServiceI {
+
+    ProductDto createProduct(ProductDto productDto);
+
+    ProductDto updateProduct(ProductDto productDto, String productId);
+
+    ProductDto getProductById(String productId);
+
+    PageableResponse<ProductDto> getAllProducts(Integer pageNumber, Integer pageSize, String sortBy, String sortDir);
+
+    PageableResponse<ProductDto>  getAllLiveProducts(Integer pageNumber, Integer pageSize, String sortBy, String sortDir);
+    void deleteProduct(String productId);
+
+    PageableResponse<ProductDto> searchProductByTitle(String subtitle,Integer pageNumber, Integer pageSize, String sortBy, String sortDir);
+
+}
