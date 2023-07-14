0e54cd9be48c80c7d5667bf81114ba6d9695f45c
MaheshSharma21 <maheshsharma127201@gmail.com>
2023-07-14 15:22:15 +0530

    AddItemToCartrequest class for inputs

--- src/main/java/com/bikkadit/electronic/store/payloads/AddItemToCartRequest.java
+++ src/main/java/com/bikkadit/electronic/store/payloads/AddItemToCartRequest.java
@@ -1,6 +1,17 @@
 package com.bikkadit.electronic.store.payloads;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class AddItemToCartRequest {
 
+    private Integer productId;
+    private Integer quantity;
 
 }
