290aa59f1a230b6862f3cad9344319579a77902c
MaheshSharma21 <maheshsharma127201@gmail.com>
2023-07-15 09:44:03 +0530

    api documentation for cart controller

--- src/main/java/com/bikkadit/electronic/store/controllers/CartController.java
+++ src/main/java/com/bikkadit/electronic/store/controllers/CartController.java
@@ -17,41 +17,62 @@ public class CartController {
     private CartServiceI cartServiceI;
 
     //add cart items to cart
+
+    /**
+     * @param request
+     * @param userId
+     * @return
+     * @apiNote This api is used to Add items to  cart
+     * @author Mahesh Sharma
+     */
     @PostMapping("/{userId}")
-    public ResponseEntity<CartDto> addItemsToCart(@RequestBody AddItemToCartRequest request , @PathVariable String userId){
+    public ResponseEntity<CartDto> addItemsToCart(@RequestBody AddItemToCartRequest request, @PathVariable String userId) {
 
         CartDto cartDto = cartServiceI.addItemsToCart(userId, request);
 
         return new ResponseEntity<>(cartDto, HttpStatus.CREATED);
     }
 
+    /**
+     * @param userId
+     * @param itemId
+     * @return
+     * @apiNote This api is used to remove items from  cart
+     * @author Mahesh Sharma
+     */
     @DeleteMapping("/{userId}/items/{itemId}")
-    public ResponseEntity<ApiResponse> removeItemFromCart( @PathVariable String userId , @PathVariable Integer itemId ){
+    public ResponseEntity<ApiResponse> removeItemFromCart(@PathVariable String userId, @PathVariable Integer itemId) {
 
-         cartServiceI.removeItemFromCart(userId ,itemId);
+        cartServiceI.removeItemFromCart(userId, itemId);
         ApiResponse response = ApiResponse.builder().message(" item removed successfully ").success(true).build();
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    /**
+     * @param userId
+     * @return
+     * @apiNote This api is used to clear  cart
+     * @author Mahesh Sharma
+     */
     @DeleteMapping("/{userId}")
-    public ResponseEntity<ApiResponse> clearCart( @PathVariable String userId ){
+    public ResponseEntity<ApiResponse> clearCart(@PathVariable String userId) {
 
-        cartServiceI.clearCart(userId );
+        cartServiceI.clearCart(userId);
         ApiResponse response = ApiResponse.builder().message("  Now cart is blank ").success(true).build();
 
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
+    /**
+     * @param userId
+     * @return
+     * @apiNote This api is used to get user specific  cart
+     * @author Mahesh Sharma
+     */
     @PostMapping("/cart/{userId}")
-    public ResponseEntity<CartDto> getCart( @PathVariable String userId ){
+    public ResponseEntity<CartDto> getCart(@PathVariable String userId) {
         CartDto cartDto = cartServiceI.getCartByUser(userId);
         return new ResponseEntity<>(cartDto, HttpStatus.OK);
     }
-
-
-
-
-
-
 }
