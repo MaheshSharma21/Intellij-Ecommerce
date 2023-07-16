8b3f002439fc4882c87d6c9d1db531c820161d69
MaheshSharma21 <maheshsharma127201@gmail.com>
2023-07-16 22:09:49 +0530

    Validation for orderItem

--- src/main/java/com/bikkadit/electronic/store/payloads/OrderItemDto.java
+++ src/main/java/com/bikkadit/electronic/store/payloads/OrderItemDto.java
@@ -5,6 +5,9 @@ import com.bikkadit.electronic.store.entities.Product;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Builder
 @Setter
@@ -14,7 +17,10 @@ import javax.persistence.*;
 public class OrderItemDto extends BaseEntityDto {
 
     private Integer orderItemId;
+
+    @NotBlank(message = " orderItem quantity is required .....")
     private int quantity;
+
     private int totalPrize;
 
     private Product product;
