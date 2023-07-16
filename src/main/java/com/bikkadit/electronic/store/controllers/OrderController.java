dae762b8a9cb46444e654ad7b57300d60276479b
MaheshSharma21 <maheshsharma127201@gmail.com>
2023-07-16 22:47:52 +0530

    api documentations perfectly apply for all controllers in Ecommerce

--- src/main/java/com/bikkadit/electronic/store/controllers/OrderController.java
+++ src/main/java/com/bikkadit/electronic/store/controllers/OrderController.java
@@ -18,10 +18,17 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class OrderController {
+
     @Autowired
     private OrderServiceI orderServiceI;
 
 
+    /**
+     * @author Mahesh Sharma
+     * @apiNote This api is used to create order
+     * @param orderDto
+     * @return
+     */
     @PostMapping("order")
     public ResponseEntity<OrderDto> createOrder(@Valid @RequestBody CreateOrderRequest orderDto) {
         OrderDto order = this.orderServiceI.createOrder(orderDto);
@@ -29,6 +36,13 @@ public class OrderController {
 
     }
 
+
+    /**
+     * @author Mahesh Sharma
+     * @apiNote This api is used to remove Order
+     * @param orderId
+     * @return
+     */
     @DeleteMapping("/order/{orderId}")
     public ResponseEntity<ApiResponse> removeOrder(@PathVariable String orderId) {
         this.orderServiceI.removeOrder(orderId);
@@ -37,6 +51,12 @@ public class OrderController {
 
     }
 
+    /**
+     * @author Mahesh Sharma
+     * @apiNote This api is used to getAllOrdersOfUser
+     * @param userId
+     * @return
+     */
     @GetMapping("/order/{userId}")
     public ResponseEntity<List<OrderDto>> getAllOrdersOfUser(@PathVariable String userId) {
         List<OrderDto> allOrdersOfUser = this.orderServiceI.getAllOrdersOfUser(userId);
@@ -44,6 +64,15 @@ public class OrderController {
 
     }
 
+    /**
+     * @author Mahesh Sharma
+     * @apiNote This api is used to getAllOrders
+     * @param pageNumber
+     * @param pageSize
+     * @param sortBy
+     * @param sortDir
+     * @return
+     */
     @GetMapping("orders")
     public ResponseEntity<PageableResponse<OrderDto>> getAllOrders(
             @RequestParam(value = AppConstant.PAGE_NUMBER, defaultValue = AppConstant.PAGE_NUMBER_DEFAULT_VALUE, required = false) int pageNumber,
