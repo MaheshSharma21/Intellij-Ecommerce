01b8c1d19decdc294605576ca090e2824b2a6998
MaheshSharma21 <maheshsharma127201@gmail.com>
2023-07-16 10:17:58 +0530

    orderItem dto attached

--- src/main/java/com/bikkadit/electronic/store/payloads/OrderItemDto.java
+++ src/main/java/com/bikkadit/electronic/store/payloads/OrderItemDto.java
@@ -1,4 +1,21 @@
 package com.bikkadit.electronic.store.payloads;
 
+import com.bikkadit.electronic.store.entities.Order;
+import com.bikkadit.electronic.store.entities.Product;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class OrderItemDto {
+
+    private Integer orderItemId;
+    private int quantity;
+    private int totalPrize;
+
+    private Product product;
 }
