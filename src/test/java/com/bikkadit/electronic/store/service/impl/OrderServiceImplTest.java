d9a341b5fa85e0e4e25c5e6bee30c76ee5913732
MaheshSharma21 <maheshsharma127201@gmail.com>
2023-07-30 10:32:35 +0530

    order service impl testing done

--- src/test/java/com/bikkadit/electronic/store/service/impl/OrderServiceImplTest.java
+++ src/test/java/com/bikkadit/electronic/store/service/impl/OrderServiceImplTest.java
@@ -1,17 +1,25 @@
 package com.bikkadit.electronic.store.service.impl;
 
 import com.bikkadit.electronic.store.entities.*;
+import com.bikkadit.electronic.store.exceptions.ResourceNotFoundException;
+import com.bikkadit.electronic.store.helper.PageableResponse;
+import com.bikkadit.electronic.store.payloads.CreateOrderRequest;
 import com.bikkadit.electronic.store.payloads.OrderDto;
 import com.bikkadit.electronic.store.repositories.CartRepository;
 import com.bikkadit.electronic.store.repositories.OrderRepository;
 import com.bikkadit.electronic.store.repositories.UserRepository;
 import com.bikkadit.electronic.store.service.OrderServiceI;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -138,13 +146,13 @@ class OrderServiceImplTest {
                 .totalPrize(15000)
                 .build();
 
-        List set = new ArrayList<>();
-        set.add(cartItem1);
-        set.add(cartItem2);
+        List lists = new ArrayList<>();
+        lists.add(cartItem1);
+        lists.add(cartItem2);
 
         cart = Cart.builder()
                 .user(user)
-                .cartItem(set)
+                .cartItem(lists)
                 .build();
 
         cart.setCreatedBy(user.getCreatedBy());
@@ -154,21 +162,79 @@ class OrderServiceImplTest {
 
     @Test
     void createOrder() {
+        CreateOrderRequest request = CreateOrderRequest.builder()
+                .userId("user123")
+                .cartId("44")
+                .billingAddress("haryana")
+                .billingPhone("983776722")
+                .billingName("Mahesh")
+                .build();
+
+        Mockito.when(userRepository.findById(Mockito.anyString())).thenReturn(Optional.of(user));
+        Mockito.when(cartRepository.findById(Mockito.anyString())).thenReturn(Optional.of(cart));
+        Mockito.when(orderRepository.save(Mockito.any())).thenReturn(order);
+        OrderDto order3 = orderService.createOrder(request);
+        Mockito.verify(cartRepository, Mockito.times(1)).save(Mockito.any());
+        System.out.println(order3);
+        Assertions.assertNotNull(order3);
+        Assertions.assertEquals(order.getBillingName(), order3.getBillingName());
+        System.out.println(order3.getBillingName());
     }
 
     @Test
     void removeOrder() {
+
+        String orderId = "123";
+        Mockito.when(orderRepository.findById(orderId)).thenReturn(Optional.of(order));
+        orderService.removeOrder(orderId);
+        Mockito.verify(orderRepository, Mockito.times(1)).delete(order);
+        Assertions.assertThrows(RuntimeException.class, () -> orderService.removeOrder("55"));
     }
 
     @Test
     void getAllOrdersOfUser() {
+        String userId = "xyz";
+        List<Order> orderList = Arrays.asList(order, order1, order2);
+
+        Mockito.when(userRepository.findById(userId)).thenReturn(Optional.of(user));
+        Mockito.when(orderRepository.findByUser(user)).thenReturn(orderList);
+        List<OrderDto> ordersOfUser = orderService.getAllOrdersOfUser(userId);
+        System.out.println(orderList.size());
+        System.out.println(ordersOfUser.size());
+        Assertions.assertEquals(orderList.size(), ordersOfUser.size());
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> orderService.getAllOrdersOfUser("abc"));
     }
 
     @Test
     void getAllOrders() {
+        List<Order> orderList = Arrays.asList(order, order1, order2);
+        Page<Order> page = new PageImpl<>(orderList);
+        Mockito.when(orderRepository.findAll((Pageable) Mockito.any())).thenReturn(page);
+        PageableResponse<OrderDto> orders = orderService.getAllOrders(1, 2, "orderDate", "asc");
+        System.out.println(page.getContent().size());
+        System.out.println(orders.getContent().size());
+        Assertions.assertEquals(page.getContent().size(), orders.getContent().size());
     }
 
     @Test
     void updateOrder() {
+        String orderId="123";
+        OrderDto orderDto= OrderDto.builder().orderAmount(12000)
+                .orderDate(new Date()).
+                orderStatus("Delivered")
+                .billingName("Mahesh")
+                .paymentStatus("PAID")
+                .billingAddress("Gurugram")
+                .billingPhone("454784")
+                .delieveryDate(null)
+                .build();
+
+
+        Mockito.when(orderRepository.findById(orderId)).thenReturn(Optional.of(order));
+        Mockito.when(orderRepository.save(order)).thenReturn(order);
+
+        OrderDto updateOrder = orderService.updateOrder(orderId,orderDto);
+        System.out.println(updateOrder.getOrderStatus());
+        Assertions.assertEquals("Delivered",updateOrder.getOrderStatus());
     }
 }
