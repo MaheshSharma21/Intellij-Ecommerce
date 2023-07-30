28cdc844ad20519c50d8452605aeb0f0a7dace9e
MaheshSharma21 <maheshsharma127201@gmail.com>
2023-07-30 09:20:45 +0530

    order service impl testing setup

---
+++ src/test/java/com/bikkadit/electronic/store/service/OrderServiceITest.java
@@ -0,0 +1,33 @@
+package com.bikkadit.electronic.store.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderServiceITest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void createOrder() {
+    }
+
+    @Test
+    void removeOrder() {
+    }
+
+    @Test
+    void getAllOrdersOfUser() {
+    }
+
+    @Test
+    void getAllOrders() {
+    }
+
+    @Test
+    void updateOrder() {
+    }
+}
