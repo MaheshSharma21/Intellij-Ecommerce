77565e850c7fb9acfa4edc542d59dc3b4d5ab916
MaheshSharma21 <maheshsharma127201@gmail.com>
2023-07-30 09:24:01 +0530

    order service impl testing working fine for create order

--- src/test/java/com/bikkadit/electronic/store/service/impl/OrderServiceImplTest.java
+++ src/test/java/com/bikkadit/electronic/store/service/impl/OrderServiceImplTest.java
@@ -1,11 +1,43 @@
 package com.bikkadit.electronic.store.service.impl;
 
+import com.bikkadit.electronic.store.entities.*;
+import com.bikkadit.electronic.store.repositories.CartRepository;
+import com.bikkadit.electronic.store.repositories.OrderRepository;
+import com.bikkadit.electronic.store.repositories.UserRepository;
+import com.bikkadit.electronic.store.service.OrderServiceI;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class OrderServiceImplTest {
+
+    @MockBean
+    private UserRepository userRepository;
+
+    @MockBean
+    private CartRepository cartRepository;
+
+    @Autowired
+    private ModelMapper mapper;
+
+    @MockBean
+    private OrderRepository orderRepository;
+
+    @Autowired
+    private OrderServiceI orderService;
+
+    private User user;
+    Product product1, product2;
+    Cart cart;
+    CartItem cartItem1, cartItem2;
+    Order order, order1, order2;
+    OrderItem orderItem1, orderItem2;
 
     @BeforeEach
     void setUp() {
