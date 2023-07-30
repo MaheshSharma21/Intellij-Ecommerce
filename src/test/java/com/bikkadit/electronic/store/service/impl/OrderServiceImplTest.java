a667837462e2fbe6a3e33392879676484ce6d17b
MaheshSharma21 <maheshsharma127201@gmail.com>
2023-07-30 09:36:28 +0530

    order service impl testing init method setup

--- src/test/java/com/bikkadit/electronic/store/service/impl/OrderServiceImplTest.java
+++ src/test/java/com/bikkadit/electronic/store/service/impl/OrderServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.bikkadit.electronic.store.service.impl;
 
 import com.bikkadit.electronic.store.entities.*;
+import com.bikkadit.electronic.store.payloads.OrderDto;
 import com.bikkadit.electronic.store.repositories.CartRepository;
 import com.bikkadit.electronic.store.repositories.OrderRepository;
 import com.bikkadit.electronic.store.repositories.UserRepository;
@@ -12,7 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @SpringBootTest
 class OrderServiceImplTest {
@@ -41,6 +43,113 @@ class OrderServiceImplTest {
 
     @BeforeEach
     void setUp() {
+        user = User.builder()
+                .name("Mahesh")
+                .email("sharma@gmail.com")
+                .password("mahesh156")
+                .gender("male")
+                .about("Testing method for create order")
+                .imageName("abc.png")
+                .build();
+
+        product1 = Product.builder()
+                .title("samsung")
+                .description("Phone having good battery and also good look")
+                .price(42000.00)
+                .discountPrice(9000.00)
+                .quantity(27)
+                .live(true)
+                .stock(false)
+                .imageName("xyz.png")
+                .build();
+
+        product2 = Product.builder()
+                .title("1+")
+                .description("Phone having good HD camera and grate pic management")
+                .price(60000.00)
+                .discountPrice(5000.00)
+                .quantity(7)
+                .live(true)
+                .stock(false)
+                .imageName("png.png")
+                .build();
+
+        orderItem1 = OrderItem.builder()
+                .product(product1)
+                .totalPrize(134000)
+                .quantity(10)
+                .build();
+
+        orderItem2 = OrderItem.builder()
+                .product(product2)
+                .quantity(5)
+                .totalPrize(150000)
+                .build();
+
+        List list = new ArrayList<>();
+        list.add(orderItem1);
+        list.add(orderItem2);
+
+        order = Order.builder().items(list).orderAmount(3000)
+                .orderDate(new Date())
+                .orderStatus("Pending")
+                .billingName("mahesh")
+                .paymentStatus("NOT_PAID")
+                .billingAddress("haryana")
+                .billingPhone("78456844")
+                .delieveryDate(null)
+                .build();
+
+        order1 = Order.builder().items(list).orderAmount(30000)
+                .orderDate(new Date())
+                .orderStatus("Pending")
+                .billingName("pankaj")
+                .paymentStatus("NOT_PAID")
+                .billingAddress("Ab.bad")
+                .billingPhone("78456844")
+                .delieveryDate(null)
+                .build();
+
+        order2 = Order.builder().items(list).orderAmount(20000)
+                .orderDate(new Date())
+                .orderStatus("Pending")
+                .billingName("kabir")
+                .paymentStatus("NOT_PAID")
+                .billingAddress("punjab")
+                .billingPhone("78456844")
+                .delieveryDate(null)
+                .build();
+        order.setCreatedBy(user.getCreatedBy());
+        order.setUpdatedBy(user.getUpdatedBy());
+        order.setIsActive(user.getIsActive());
+
+        List<Order> orderList = Arrays.asList(order, order1, order2);
+        List<OrderDto> dtoList = orderList.stream().map(e -> mapper.map(e, OrderDto.class)).toList();
+
+        cartItem1 = CartItem.builder()
+                .product(product1)
+                .quantity(9)
+                .totalPrize(13400)
+                .build();
+
+        cartItem2 = CartItem.builder()
+                .product(product2)
+                .quantity(11)
+                .totalPrize(15000)
+                .build();
+
+        List set = new ArrayList<>();
+        set.add(cartItem1);
+        set.add(cartItem2);
+
+        cart = Cart.builder()
+                .user(user)
+                .cartItem(set)
+                .build();
+
+        cart.setCreatedBy(user.getCreatedBy());
+        cart.setUpdatedBy(user.getUpdatedBy());
+        cart.setIsActive(user.getIsActive());
     }
 
     @Test
